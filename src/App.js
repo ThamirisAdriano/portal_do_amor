7a02481c992ef7fd9198b74fb78f9ff5736ddcd0
Thiago S Adriano <tadriano.net@gmail.com>
2022-12-14 19:55:17 -0300

    subindo ajustes para forma

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import MaskedFormControl from 'react-bootstrap-maskedinput'
+import axios from 'axios';
+
+
 
 function App() {
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    const { nickname, birthday } = e.target.elements
+
+    var obj = {};
+    obj['nickname'] = nickname.value;
+    obj['birthday'] = birthday.value;
+
+    console.log(obj);
+
+    axios.post('', obj)
+      .then((data) => {
+        if (data.status == 200) {
+          alert('Cadastro enviado com sucesso!')
+        }
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container >
+      <Form onSubmit={handleSubmit}>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formGridNickname">
+            <Form.Label>Nome</Form.Label>
+            <Form.Control type="text" id="nickname" name="Name" placeholder="Nome" />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridBirthday">
+            <Form.Label>Data de Nascimento</Form.Label>
+            <MaskedFormControl type='text' id="birthday" name='birthday' mask='11/11/1111' />
+          </Form.Group>
+        </Row>
+
+        <Button variant="primary" type="submit">
+          Enviar
+        </Button>
+      </Form>
+    </Container>
   );
 }
 
